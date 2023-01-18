1da0053f44b5728dbbda21d38546d4a7685052e7
Sumio K <tap.miwo@gmail.com>
2023-01-19 06:09:46 +0900

    to 30

---
+++ no21-30.py
@@ -0,0 +1,49 @@
+# 21
+m = 5
+m += 2
+print(m)
+
+# 22
+num = 1
+print(num)
+
+# 23
+n = 'Python'
+print(n)
+print('n')
+
+# 24
+n = 'ぱいそん'
+print(n)
+n = 'Python'
+print(n)
+
+# 25
+n = 3 + 7
+print(n)
+
+# 26
+x = 2
+print(x * 5)
+
+# 27
+n = 4
+print(n)
+n *= 5
+print(n)
+
+# 28
+a = 10
+b = 20
+a, b = b, a
+print(a, b)
+
+# 29
+a, b = 10, 20
+print(a)
+print(b)
+
+# 30
+a = 10
+b = 2
+print(a + b, a - b, a * b, a / b)
