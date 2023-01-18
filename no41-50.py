414776a725aac997fa62c003b6dc555ff86fc278
Sumio K <tap.miwo@gmail.com>
2023-01-19 06:37:25 +0900

    to 50

---
+++ no41-50.py
@@ -0,0 +1,49 @@
+# 41
+print(int('123'))
+print(int('456'))
+print(int('123') + int('456'))
+
+# 42
+print(str(123))
+print(str(456))
+print(str(123) + str(456))
+
+# 43
+p = '10'
+print(p * 3)
+
+# 44
+a = 3.14
+b = 10
+print(a + b)
+
+# 45
+a = '34'
+b = '43'
+print(int(a) + int(b))
+
+# 46
+print(4 + 6 != -10)
+
+# 47
+print(4 + 6 <= -10)
+
+# 48
+n = 16
+if n > 15:
+  print('とても大きい数字')
+
+# 49
+n = 15
+m = 15
+if n > 15:
+  print('nはとても大きい数字')
+if m <= 15:
+  print('mは小さい数字')
+
+# 50
+n = 14
+if n > 15:
+  print('nは大きい数字')
+else:
+  print('小さい数字')
