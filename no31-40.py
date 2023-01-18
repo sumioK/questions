1bb6e20a9c2fbcfdfa7f4d2454582043bc21ddf1
Sumio K <tap.miwo@gmail.com>
2023-01-19 06:21:58 +0900

    to 40

---
+++ no31-40.py
@@ -0,0 +1,42 @@
+# 31
+a = 11
+b = 3
+print(a // b)
+
+# 32
+a = 5
+b = 3
+print(a % b)
+
+# 33
+a = 2
+b = 5
+print(a ** b)
+
+# 34
+p = '東京'
+print('私は' + p + '出身です')
+
+# 35
+p = '東京'
+n = 10 + 10
+print('私は'+ p +'出身です。'+ str(n) +'歳です')
+
+# 36
+print('Hello' + 'World')
+
+# 37
+print('Hello' * 10)
+
+# 38
+s = 'Hello'
+s += 'World'
+print(s)
+
+# 39
+s = "hello"
+s *= 10
+print(s)
+
+# 40
+print(type(10 + 10))
