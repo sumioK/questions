e68226e51ca09ffe5012db840d893d0140bba3a2
Sumio K <tap.miwo@gmail.com>
2023-01-18 21:19:20 +0900

    10

---
+++ no1-.py
@@ -0,0 +1,35 @@
+# 1
+print('Hello world')
+
+# 2
+print('hello')
+
+# 3
+print("World")
+
+# 4
+# print(3 + 8)
+
+# 5
+print(3 + 8)#comments
+
+# 6
+print('ABC')
+'''
+print('DEF')
+print('GHI')
+'''
+print('JKL')
+
+# 7
+print(18)
+print(2+6)
+
+#8
+print('2+6')
+
+# 9
+print(5+3)
+
+#10
+print(5 * 3)
