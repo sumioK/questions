7530151622a9f6079ae9313e5027cad899d0dd92
Sumio K <tap.miwo@gmail.com>
2023-01-20 06:55:46 +0900

    to 63

---
+++ no61-70.py
@@ -0,0 +1,22 @@
+# 61
+s1 = "Hello Python"
+s2 = "Hello"
+if s1.startswith(s2):
+  print(s1 + "は" + s2 + "から始まる")
+
+# 62
+n1 = 14
+n2 = 28
+if n1 > 8 and n1 < 14:
+  print(True)
+else:
+  print(False)
+if n1 ** 2 >= n2 * 5:
+  print(True)
+else:
+  print(False)
+
+# 63
+s = "python"
+print(s[3])
+
