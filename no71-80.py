c987631387a07b4506804ccd360d8c967ba209cf
Sumio K <tap.miwo@gmail.com>
2023-01-21 06:57:36 +0900

    to75

---
+++ no71-80.py
@@ -0,0 +1,23 @@
+# 71
+world = "Hello"
+print(world[2:4])
+
+# 72
+s = 'training'
+print(s[1:5])
+
+# 73
+s = "understand"
+n = [1, 3, 5, 7, 9]
+for i in n:
+  print(s[i])
+
+print(s[1::2])
+
+# 74
+li = [1, 2, 3, 4, 5]
+print(li[::-1])
+
+# 75
+mat = [[1, 2], [3, 4]]
+print(mat[0][1])
