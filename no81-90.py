f1896bf5bbc12aa4541de0d024841571a87ba6ae
Sumio K <tap.miwo@gmail.com>
2023-01-22 17:50:34 +0900

    to 85

---
+++ no81-90.py
@@ -0,0 +1,26 @@
+# 81
+li = [1, 2, 3, 4, 5]
+li.append(6)
+li.append(7)
+print(li)
+
+# 82
+li = [1, 2, 3, 4, 5]
+li.append(6)
+print(li)
+
+# 83
+li = [1, 2, 3, 4, 5]
+li[1:4] = [10, 11, 12]
+
+print(li)
+
+# 84
+li = [1, 2, 3, 4, 5]
+del li[1]
+print(li)
+
+# 85
+li = [3, 2, 1, 5, 4]
+li.pop()
+print(li)
