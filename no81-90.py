2a65e91ef4f74ac41e4c1b7c3dda3bcaedc9d40b
Sumio K <tap.miwo@gmail.com>
2023-01-22 18:02:09 +0900

    to 90

--- no81-90.py
+++ no81-90.py
@@ -24,3 +24,26 @@ print(li)
 li = [3, 2, 1, 5, 4]
 li.pop()
 print(li)
+
+# 86
+li = [1, 2, 3, 4, 5]
+del li[1:4]
+print(li)
+
+# 87
+li = [1, 2, 3, 4, 5]
+li.insert(1,100)
+print(li)
+
+# 88
+li = ['orange', 'apple', 'banana', 'pieapple', 'lemon', 'apple', 'banana']
+print(li.count('apple'))
+
+# 89
+li = ['orange', 'apple', 'banana', 'pineapple,', 'lemon', 'apple', 'bnana']
+print(li.index('lemon'))
+
+# 90
+li = ['orange', 'apple', 'banana', 'pineapple,', 'lemon', 'apple', 'bnana']
+li.reverse()
+print(li)
