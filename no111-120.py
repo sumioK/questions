d57f3b7597e8c2608ec8e1865e8fe58a6e8bfc7b
Sumio K <tap.miwo@gmail.com>
2023-01-22 21:45:18 +0900

    to 92

---
+++ no111-120.py
@@ -0,0 +1,10 @@
+# 111
+town = {'Kanagawa': 'Yokohama', 'Hokkaido': 'Sapporo', 'Okinawa': 'Naha'}
+for s in town:
+  print(s)
+
+# 112
+town = {'Kanagawa': 'Yokohama', 'Hokkaido': 'Sapporo', 'Okinawa': 'Naha'}
+for k, v in town.items():
+  print(f'{k}は{v}です')
+
