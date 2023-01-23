20282881da7a4bdcbe73596b89979c21bf006e3e
Sumio K <tap.miwo@gmail.com>
2023-01-23 21:20:42 +0900

    to 120

--- no111-120.py
+++ no111-120.py
@@ -8,3 +8,32 @@ town = {'Kanagawa': 'Yokohama', 'Hokkaido': 'Sapporo', 'Okinawa': 'Naha'}
 for k, v in town.items():
   print(f'{k}は{v}です')
 
+# 114 **
+town = {'Kanagawa': 'Yokohama', 'Hokkaido': 'Sapporo', 'Okinawa': 'Naha'}
+print('Kanagawa' in town)
+
+# 115 ***
+town = {'Kanagawa': 'Yokohama', 'Hokkaido': 'Sapporo', 'Okinawa': 'Naha'}
+print('Kanagawa' in town.values())
+
+# 116
+d = {'x': 10, 'y': 20}
+print(d)
+
+# 117
+d = dict(x = 10, y = 20)
+print(d)
+
+# 118
+d = {'x': 10, 'y': 20}
+print(type(d))
+
+# 119
+d = {'x': 10, 'y': 20}
+print(d['x'])
+
+# 120
+d = {'x': 10, 'y': 20}
+d['x'] = 100
+print(d)
+
