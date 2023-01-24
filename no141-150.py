e808e9b36a9f25c509e940e65c5f4e7340ac5a7d
Sumio K <tap.miwo@gmail.com>
2023-01-25 06:32:06 +0900

    to 150

--- no141-150.py
+++ no141-150.py
@@ -22,3 +22,29 @@ print(s1 - s2)
 s1 = {1, 2, 4, 5}
 s2 = {2, 3, 4, 6}
 print(s1 ^ s2)
+
+# 146
+s1 = {1, 2, 3, 4, 5}
+s2 = {3, 4, 5, 6, 7}
+print(s1 | s2)
+
+# 147
+s1 = {1, 2, 3, 4, 5}
+s2 = {3, 4, 5, 6, 7}
+print(s1 - s2)
+
+# 148
+data1 = {'A': 1, 'B': 2}
+data2 = 'hoge'
+data3 = {1, 2, 3, 4, 5}
+print(type(data1))
+print(type(data2))
+print(type(data3))
+
+# 149
+t = (123, '456')
+print(t)
+
+# 150
+t = 123, '456'
+print(t[0])
