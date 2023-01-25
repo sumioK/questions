228bce1346b439a245d88567ec17806da6163e29
Sumio K <tap.miwo@gmail.com>
2023-01-26 07:02:27 +0900

    to 178

--- no171-180.py
+++ no171-180.py
@@ -24,3 +24,20 @@ word = "WORD"
 num = '0123'
 for n , w in zip(num, word):
   print("{}-{}".format(n, w))
+
+# 176
+li = [5, 4, 3, 2, 1]
+ans_li = [idx + elem for idx, elem in enumerate(li)]
+print(ans_li)
+
+# 177
+li = [11, 22, 33, 44, 55]
+if 44 in li:
+  print(True)
+else:
+  print(False)
+
+# 178
+li = [1, 2, 3, 4, 5]
+t = (li[0], li[-1])
+print(t)
