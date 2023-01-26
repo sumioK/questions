8ef8e8c1bdc309ccb75e6c3d53f3a4196c62d558
Sumio K <tap.miwo@gmail.com>
2023-01-27 06:13:19 +0900

    to 185

---
+++ no181-190.py
@@ -0,0 +1,23 @@
+# 181
+r = [x for x in range(10)]
+print(r)
+
+# 182
+print([i*5 for i in range(1, 10)])
+
+# 183
+print([i*2 for i in range(5)])
+print([i for i in range(10) if i %2 == 0])
+
+# 184
+A = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+x =[i*10 for i in A]
+print(x)
+
+# 185
+x = [1, 2, 3]
+y = [1, 2, 3]
+a = [[x, y] for x in [1, 2, 3] for y in [1, 2, 3]]
+print(a)
+
+# 
