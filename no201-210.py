c9d5377299b151080eb856bff0435fb0ec8a4295
Sumio K <tap.miwo@gmail.com>
2023-01-28 21:03:22 +0900

    to 210

---
+++ no201-210.py
@@ -0,0 +1,41 @@
+# 201
+A = 'abc'
+B = 'def'
+print(A, B)
+
+# 202
+A = 'abc'
+B = 'def'
+print(A, B, sep=':')
+
+# 203
+print('hello', end = '')
+print('world')
+
+# 204
+s = "ABC DEF GHI"
+print(s.find('D'))
+
+# 205
+s = 'DEF DEF GHI'
+print(s.find('DEF'))
+
+# 206
+s = 'ABC DEF GHI'
+print(s.find('DEF'))
+
+# 207
+s = 'ABC DEF GHI'
+print(s.find('ZXY'))
+
+# 208
+# s = 'ABC DEF GHI'
+# print(s.index('ZXY'))
+
+# 209
+s = 'Hello'
+print(s.rjust(10))
+
+# 210
+word = 'Japan tokyo'
+print(len(word))
