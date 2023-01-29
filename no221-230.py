3a8ec56e904b522799babab3a8b2f4725d95f890
Sumio K <tap.miwo@gmail.com>
2023-01-29 19:11:15 +0900

    lower

---
+++ no221-230.py
@@ -0,0 +1,19 @@
+# 221
+a = 5
+b = 3
+print('{}%{}={}'.format(a, b, a % b))
+
+# 222
+print('Hello Python'.replace('Python', 'World'))
+
+# 223
+s = 'some1'
+print(s.replace('1', 'one'))
+
+# 224
+n = [3, 6, 3, 8, 5, 2, 4, 6]
+print(n.index(2))
+
+# 225
+s = 'This Is A Sentence'
+print(s.lower())
