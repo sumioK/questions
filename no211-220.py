1f314483783178355458ff041d0579717e25f04e
Sumio K <tap.miwo@gmail.com>
2023-01-29 18:56:24 +0900

    to 215

---
+++ no211-220.py
@@ -0,0 +1,19 @@
+# 211
+A = 'Potato'
+print(len(A))
+
+# 212
+n = [4, 5, 2, 7, 6]
+print(len(n))
+
+# 213
+li = [1, 2, 3, 4, 5]
+print(len(li))
+
+# 214
+s = 'How many characters?'
+print(len(s))
+
+# 215
+word = 'word'
+print(word.upper())
