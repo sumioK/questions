e77a46785bd62978569a4bde4165061d21311736
Sumio K <tap.miwo@gmail.com>
2023-01-29 19:04:09 +0900

    to 220

--- no211-220.py
+++ no211-220.py
@@ -17,3 +17,27 @@ print(len(s))
 # 215
 word = 'word'
 print(word.upper())
+
+# 216
+A = 'ONONOIMOKO'
+print(A.count('O'))
+
+# 217
+i = -10
+print(abs(i))
+
+# 218
+a = 0.5
+b = '0.4'
+i = float(b)
+print(a * i)
+
+# 219
+summer = '夏'
+hot = '暑い'
+print('{}は{}'.format(summer, hot))
+
+# 220
+summer = '夏'
+hot = '暑い'
+print('{1}は{0}'.format(summer, hot))
